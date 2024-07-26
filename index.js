c21a7a76b2cebb5f8895e86887f8897b2905eef9
Ronald Sayre III <115036926+RJ3605@users.noreply.github.com>
2024-07-26 14:28:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ import FetchWrapper from "./fetchwrapper.js";
 const countriesList = document.querySelector(".countries-list");
 const API = new FetchWrapper("https://restcountries.com/v3.1");
 const page = document.querySelector(".page");
-const countries = document.querySelectorAll(".country");
+const detailsPage = document.querySelector(".country-details-page");
 const region = document.querySelector(".region");
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const toggleIcon = document.querySelector("#toggle-icon");
 const searchInput = document.querySelector("#search");
+const backButton = document.querySelector("#back-button");
 let allData;
 
-/*Built country card*/
+// Builds country card
 function countryCard(countryData) {
   countriesList.insertAdjacentHTML(
     "beforeend",
     `<div class="country">
-        <img src="${countryData.flags.png}" alt="${
-      countryData.name.official
+        <img src="${countryData.flags.png}" class="${countryData.cca3}"alt="${
+      countryData.name.common
     } Flag">
-        <div class="details">
-            <h2>${countryData.name.official}</h2>
+        <div class="summary ${countryData.cca3}">
+            <h2>${countryData.name.common}</h2>
             <p><strong>Population:</strong> ${countryData.population.toLocaleString()}</p>
             <p><strong>Region:</strong> ${countryData.region}</p>
             <p><strong>Capital:</strong> ${countryData.capital}</p>
@@ -28,12 +29,144 @@ function countryCard(countryData) {
   );
 }
 
-/*Get all data */
+// Finds the name of border countries
+function findBorderCountryName(borderCountryCode) {
+  allData.forEach((country) => {
+    if (country.cca3 === borderCountryCode) {
+      document.querySelector(`button#${borderCountryCode}`).innerText =
+        country.name.common;
+    }
+  });
+}
+
+// Builds detailed country page
+function buildDetailPage(countryData) {
+  page.display = "none";
+  detailsPage.innerHTML = "";
+  detailsPage.insertAdjacentHTML(
+    "beforeend",
+    `<div id="image-div">
+                    <img src="${countryData.flags.png}" alt="${
+      countryData.name.official
+    } Flag" class="details-flag ${countryData.cca3}">
+                </div>
+                <div class="country-details-page-subcontainer">
+                <div class="country-details-page-sub-subcontainer">
+                  <h2>${countryData.name.common}</h2>
+                    <div class="country-details-page-list">
+                        <p><strong>Official Name:</strong> ${
+                          countryData.name.official
+                        }</p>
+                        <p><strong>Population:</strong> ${countryData.population.toLocaleString()}</p>
+                        <p><strong>Region:</strong> ${countryData.region}</p>
+                        <p><strong>Subregion:</strong> ${
+                          countryData.subregion
+                        }</p>
+                        <p><strong>Capital:</strong> ${countryData.capital}</p>
+                        <p><strong>Top Level Domain:</strong> ${
+                          countryData.tld
+                        }</p>
+                        <p class="currencies"><strong>Currencies:</strong> </p>
+                        <p class="languages"><strong>Languages:</strong> </p>
+                    </div>
+                    </div>
+                    <div class="border-countries-container">
+                        <p><strong>Border Countries:</strong></p>
+                        <div class="border-countries">
+                        </div>
+                    </div>
+                </div>
+    `
+  );
+  const countryDetailsPageCurrencies = document.querySelector(".currencies");
+  const countryDetailsPageLanguages = document.querySelector(".languages");
+  const countryDetailsPageBorders = document.querySelector(".border-countries");
+
+  // Populate currencies
+  if (countryData.currencies) {
+    const currencies = Object.values(countryData.currencies);
+
+    currencies.forEach((currency, index) => {
+      if (index === 0) {
+        countryDetailsPageCurrencies.insertAdjacentHTML(
+          "beforeend",
+          `${currency.name}`
+        );
+      } else {
+        countryDetailsPageCurrencies.insertAdjacentHTML(
+          "beforeend",
+          `, ${currency.name}`
+        );
+      }
+    });
+  } else {
+    countryDetailsPageCurrencies.insertAdjacentHTML("beforeend", "None");
+  }
+
+  // Populate languages
+  if (countryData.languages) {
+    const languages = Object.values(countryData.languages);
+
+    languages.forEach((language, index) => {
+      if (index === 0) {
+        countryDetailsPageLanguages.insertAdjacentHTML(
+          "beforeend",
+          `${language}`
+        );
+      } else {
+        countryDetailsPageLanguages.insertAdjacentHTML(
+          "beforeend",
+          `, ${language}`
+        );
+      }
+    });
+  } else {
+    countryDetailsPageLanguages.insertAdjacentHTML("beforeend", "None");
+  }
+
+  // Populate border countries
+  if (countryData.borders) {
+    const borderCountries = Object.values(countryData.borders);
+    borderCountries.forEach((border) => {
+      countryDetailsPageBorders.insertAdjacentHTML(
+        "beforeend",
+        `<button class="border-country" id="${border}">${border}</button>`
+      );
+      findBorderCountryName(border);
+    });
+  } else {
+    countryDetailsPageBorders.insertAdjacentHTML(
+      "beforeend",
+      `<button class="border-country" id="none">None</button>`
+    );
+  }
+
+  page.style.display = "none";
+  backButton.style.display = "block";
+  detailsPage.style.display = "flex";
+}
+
+// Border country button to recreate details page with border country information
+document.body.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.className === "border-country") {
+      allData.forEach((countryData) => {
+        if (event.target.id === countryData.cca3) {
+          buildDetailPage(countryData);
+        }
+      });
+    }
+  },
+  false
+);
+
+//Get all data
 const countriesAll = () =>
   API.get("/all").then((data) => {
-    const countriesData = data;
+    const allCountriesData = data;
     allData = data;
-    countriesData.forEach((countryData) => {
+    allCountriesData.forEach((countryData) => {
       countryCard(countryData);
     });
   });
@@ -50,7 +183,7 @@ const countriesByRegion = (currentRegion) =>
     });
   });
 
-// Region Filter
+// Region Filter functionality
 function filterByRegion(selectedRegion) {
   selectedRegion = region.value;
   if (selectedRegion !== "") {
@@ -66,22 +199,49 @@ region.addEventListener("change", function () {
   filterByRegion();
 });
 
-// Search Bar
+// Search Bar functionality
 searchInput.addEventListener("input", function (e) {
   let resultData = allData.filter(function (country) {
     let chosen = country.name.common.toLowerCase();
-    let searchCountryName = e.target.value.toLowerCase();
-    ;
-    return chosen.includes(searchCountryName.trim());
+    let searchedCountryName = e.target.value.toLowerCase().trim();
+    return chosen.includes(searchedCountryName);
   });
   countriesList.innerHTML = "";
-  console.log(resultData);
   resultData.forEach((countryData) => {
     countryCard(countryData);
   });
 });
 
-/* Create a function that loads a page for each country when their summary is clicked. */
+// Build details page on click
+document.body.addEventListener(
+  "click",
+  function (event) {
+    if (
+      event.target.parentElement.className === "country" ||
+      event.target.parentElement.parentElement.className === "country"
+    ) {
+      console.log(event);
+      allData.forEach((countryData) => {
+        if (event.target.classList.contains(countryData.cca3)) {
+          buildDetailPage(countryData);
+        } else if (
+          event.target.parentElement.classList.contains(countryData.cca3)
+        ) {
+          buildDetailPage(countryData);
+        }
+      });
+    }
+  },
+  false
+);
+
+// Back button functionality
+backButton.onclick = () => {
+  detailsPage.style.display = "none";
+  backButton.style.display = "none";
+  detailsPage.innerHTML = "";
+  page.style.display = "flex";
+};
 
 // Dark Mode
 function toggleDarkLightMode(isLight) {
